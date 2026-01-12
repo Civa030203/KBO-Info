10a0e1980921d166847c56b38a5ce753889a8f76
Seung-Jea Joo / 주승제 / 朱昇濟 <seungjeajoo@gmail.com>
2026-01-12 12:48:15 +0900

    Updated 'relay.js'

--- kbo-rankings/src/pages/relay.js
+++ kbo-rankings/src/pages/relay.js
@@ -215,7 +215,9 @@ export default function LiveTextPage() {
                   <ul className="mt-2 space-y-2" key={resIdx}>
                     <li className="p-2 border-b last:border-none text-sm">
                       {parseInt(scoreData.resultData[0][0]) > parseInt(scoreData.resultData[1][0]) && inning.TB_SC === 'B' ? 
-                        <span>{res.LIVETEXT_IF}</span> : <p></p>
+                        <span>{res.LIVETEXT_IF}</span> :
+                          parseInt(scoreData.resultData[0][0]) < parseInt(scoreData.resultData[1][0]) && inning.TB_SC === 'T' ?
+                            <span>{res.LIVETEXT_IF}</span> : <span></span>
                       }
                     </li>
                   </ul>
